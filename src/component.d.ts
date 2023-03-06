f0f5a4205c84c5a54515924d7868df664ffab464
Yang <olaysunju@163.com>
2023-03-06 17:59:38 +0800

    chore: set git repository

--- src/component.d.ts
+++
@@ -1,9 +0,0 @@
-import ValidateForm from "./components/ValidateForm/index.vue";
-import ValidateFormItem from "./components/ValidateFormItem/index.vue";
-
-declare module "@vue/runtime-core" {
-  export interface GlobalComponents {
-    ValidateForm: typeof ValidateForm;
-    ValidateFormItem: typeof ValidateFormItem;
-  }
-}
